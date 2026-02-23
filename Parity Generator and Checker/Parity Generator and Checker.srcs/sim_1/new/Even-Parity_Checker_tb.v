124d94198e715cdeee6a76df614208dc9fe669aa
ranasiddu18 <markasiddu18@gmail.com>
2026-02-23 17:16:52 +0530

    Parity Checker added

---
+++ Parity Generator and Checker/Parity Generator and Checker.srcs/sim_1/new/Even-Parity_Checker_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.02.2026 11:59:05
+// Design Name: 
+// Module Name: Even-Parity_Checker_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Even-Parity_Checker_tb(
+
+    );
+endmodule
